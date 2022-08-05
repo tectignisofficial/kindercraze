0ba6af274428989381cea54630c409db79dc1ec3
Madhuri Dere <madhuridere2018@gmail.com>
2022-08-05 11:23:07 +0530

    chnages

---
+++ include/senior_kg.php
@@ -0,0 +1,256 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>Kinder Craze Schools  &mdash; Tectignis </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700|Indie+Flower" rel="stylesheet">
+    
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+      <?php
+     include("include/header.php");
+     ?>
+
+
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container mb-3">
+          <div class="d-flex align-items-center">
+            <div class="site-logo mr-auto">
+              <a href="index.html">Kinders Craze <span class="text-primary">.</span></a>
+            </div>
+            <div class="site-quick-contact d-none d-lg-flex ml-auto ">
+              <div class="d-flex site-info align-items-center mr-5">
+                <span class="block-icon mr-3"><span class="icon-map-marker text-yellow"></span></span>
+                <span>Aashiyana CHS Shop No 05,Sector 11, Plot No 29, <br> Kamothe, Navi Mumbai, Maharashtra 410206</span>
+              </div>
+              <div class="d-flex site-info align-items-center">
+                <span class="block-icon mr-3"><span class="icon-clock-o"></span></span>
+                <span>Sunday - Friday 8:00AM - 4:00PM <br> Saturday CLOSED</span>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+
+        <div class="container">
+          <div class="menu-wrap d-flex align-items-center">
+            <span class="d-inline-block d-lg-none"><a href="#" class="text-black site-menu-toggle js-menu-toggle py-5"><span class="icon-menu h3 text-black"></span></a></span>
+
+              
+
+              <nav class="site-navigation text-left mr-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav mr-auto ">
+                  <li><a href="index.html" class="nav-link">Home</a></li>
+                  <li><a href="about.html" class="nav-link">About</a></li>
+                  <li><a href="packages.html" class="nav-link">Packages</a></li>
+                  <li><a href="gallery.html" class="nav-link">Gallery</a></li>
+                  <li class="active"><a href="pricing.html" class="nav-link">Pricing</a></li>
+                  <li><a href="contact.html" class="nav-link">Contact</a></li>
+                </ul>
+              </nav>
+
+              <div class="top-social ml-auto">
+                <a href="https://facebook.com/"><span class="icon-facebook text-teal"></span></a>
+                <a href="https://twitter.com/"><span class="icon-twitter text-success"></span></a>
+                <a href="https://instagram.com/"><span class="icon-instagram text-pink"></span></a>
+              </div>
+          </div>
+        </div>
+
+       
+
+      </header>
+
+    <div class="ftco-blocks-cover-1">
+      <div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('images/kids.jpg')">
+        <div class="container">
+          <div class="row align-items-center ">
+
+            <div class="col-md-5 mt-5 pt-5">
+              <span class="text-cursive h5 text-red">Terms and Service</span>
+              <h1 class="mb-3 font-weight-bold text-teal">terms and service</h1>
+              <p><a href="index.html" class="text-white">Home</a> <span class="mx-3">/</span> <strong>terms</strong></p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <span class="text-cursive h5 text-red d-block">Terms And Service</span>
+            <h2 class="text-black">Terms Us</h2>
+            <p>You can only contract our services and validly consent to our Terms and servises if you are of legal age.
+
+            </p>
+          </div>
+          <div class="col-md-4">
+            <div class="pricing teal">
+              
+              <h3>terms and Services</h3>
+              <ul class="ul-check list-unstyled teal">
+                <li>Our products are directed to people of legal age. In specific cases, we may explicitly allow them to be hired for use by minors. They must be examined and accepted, on the minor behalf, by the person of legal age who is the holder of the parental authority or in charge of their custody or education. </li>  
+                
+              </ul>
+             
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="pricing danger">
+             
+          
+              <h3>Services</h3>
+              <ul class="ul-check list-unstyled danger">
+                <li>we offer LKG Class Education Services, Play Group Education Service, UKG Class Education Services, Nursery Class Education Services, Pre Nursery Class Education Services and Junior Kg Class Education Service from India.</li>  
+                
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 text-center">
+            <span class="text-cursive h5 text-red d-block">Testimonial</span>
+            <h2 class="text-black">Word of mouth has been the driver of our success for over 30 years. Here are some of the great things people have said about us:</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+            <div class="testimonial-3-wrap">
+              
+
+              <div class="owl-carousel nonloop-block-13">
+                <div class="testimonial-3 d-flex">
+                  <div class="vcard-wrap mr-5">
+                    <img src="images\vedant.jpg" alt="Image" class="vcard img-fluid">
+                  </div>
+                  <div class="text">
+                    <h3>Vedant Naidu</h3>
+                    <p class="position">Partner</p>
+                    <p>My daughter has been going to this school for more than one year and she loves all the teachers in the school . Teachers are so caring and lovable. She likes the extra curricular activities such as tinkering lab, painting and gardening. In addition to extra curricular activities they also teach well. She learns new stuff everyday and her communication has improved a lot. This school has turned my daughter into a happy kid.</p>
+                  </div>
+                </div>
+
+                <div class="testimonial-3 d-flex">
+                  <div class="vcard-wrap mr-5">
+                    <img src="images\craze.jpg" alt="Image" class="vcard img-fluid">
+                  </div>
+                  <div class="text">
+                    <h3>Monika Gore</h3>
+                    <p class="position">Partner</p>
+                    <p>This is our 5th year at CMP and we couldn’t be happier for being part of this amazing school! This is truly a place where kids can be kids: they will play in puddles when it’s raining (while using appropriate gear – rain boots and jackets), they will play in the mud kitchen, they will have so much fun! The outdoor activities are our favorites: Nature Lab, Chicken Coop, outdoor decks, Tinkering, Art Studio and Music Garden.</p>
+                  </div>
+                </div>
+
+                <div class="testimonial-3 d-flex">
+                  <div class="vcard-wrap mr-5">
+                    <img src="images\logo22.jpg" alt="Image" class="vcard img-fluid">
+                  </div>
+                  <div class="text">
+                    <h3>Dipali Kale</h3>
+                    <p class="position">Partner</p>
+                    <p>We have been sending our son to Atharva Kale Preschool for 3 years now and the progress in development we see in our son is just a joy. He learns so much new stuff and words that it sometime raises an eyebrow, a very positive one, such as where did you get that word from, unexpected but very perfect and accurate description.</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5 justify-content-center">
+
+          <div class="col-md-8">
+            
+
+            <div class="row">
+              <div class="col-lg-3 text-center">
+                <span class="text-teal h2 d-block">4450</span>
+                <span>Happy Client</span>
+              </div>
+              <div class="col-lg-3 text-center">
+                <span class="text-yellow h2 d-block">4398</span>
+                <span>Members</span>
+              </div>
+              <div class="col-lg-3 text-center">
+                <span class="text-success h2 d-block">50+</span>
+                <span>Staffs</span>
+              </div>
+              <div class="col-lg-3 text-center">
+                <span class="text-danger h2 d-block">1500+</span>
+                <span>Our Followers</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+    <?php
+    include("include/footer.php");
+    ?>
+
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+  </body>
+
+</html>
+
