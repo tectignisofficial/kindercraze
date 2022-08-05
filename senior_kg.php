40b4dc3d0c7f831e4d8d4a145021309729285eda
Madhuri Dere <madhuridere2018@gmail.com>
2022-08-05 12:37:57 +0530

    chnages

---
+++ senior_kg.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>Kinder Craze Schools  &mdash; Tectignis </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700|Indie+Flower" rel="stylesheet">
+    
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+      <?php
+     include("include/header.php");
+     ?>
+
+
+     
+    <div class="ftco-blocks-cover-1">
+      <div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('images/kids.jpg')">
+        <div class="container">
+          <div class="row align-items-center ">
+
+            <div class="col-md-5 mt-5 pt-5">
+              <span class="text-cursive h5 text-red">Senior KG</span>
+              <h1 class="mb-3 font-weight-bold text-teal">Senior KG</h1>
+              <p><a href="index.html" class="text-white">Home</a> <span class="mx-3">/</span> <strong>Senior KG</strong></p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="pricing pricing-1 pt-100 ">
+        <div class="container">
+            <div class="default-section-title default-section-title-middle pt-5" style="text-align:center;" >
+                
+                <h2>SINIOR-KG</h2>
+            </div>
+            <div class="setion-content">
+                            <div class="row justify-content-center">
+                            <div class="col-8">
+                        <label>Senior KG offers children the chance to display their potentials. By now they are aware of the opportunities offered by their environment and use them to the fullest. They are exposed to wider vocabulary and can express themselves well. They are conscious of their self-esteem and are now more confident. They are exposed to simple addition and subtraction. They are to learn combinations of primary colors to form secondary colours. They are made to study patterns and continue with the next combination. They are to learn logical reasoning through solving simple puzzles. They are to learn reading using rhyming words. They are taking out of the school on field trips to enable them explore their environment to the fullest.</label>
+                </div>
+            </div>
+                        <div class="row justify-content-center pt-3">
+                        <div class="col-8">
+              <h4>LEARNING SCHOOLS</h4>
+            <li>  Fine Motor Activities</li>
+            <li>  Writing , Reading , Math Skills</li>
+            <li>  Imaginative and Free Play</li>
+            </div>
+            </div>
+                         <div class="row justify-content-center pt-3">
+                         <div class="col-8">
+                         <h4>PERCEPTUAL MOTOR PROGRAMME</h4>
+              <li>    Music and Movement</li>
+              <li>    Sand and Water Play</li>
+              <li>    Organized Games</li>
+              </div>
+            </div>
+
+            <div class="row justify-content-center pt-3">
+                         <div class="col-8">
+                         <h4>CONVERSATION</h4>
+              <li>  Listening and Speaking Skills</li>
+              <li>   Reading Skills</li>
+             
+              </div>
+            </div>
+
+            <div class="row justify-content-center pt-3">
+                         <div class="col-8">
+                         <h4>LANGUAGE SKILLS</h4>
+              <li>   Reading Skill</li>
+              <li>   Writing Skills</li>
+             
+              </div>
+            </div>
+
+            <div class="row justify-content-center pt-3">
+                         <div class="col-8">
+                         <h4>CREATIVITY</h4>
+              <li>  Freehand Drawing, Tracing and Joining Dots</li>
+              <li>   Artists</li>
+              <li>   Art and Craft</li>
+              <li>   Colouring, Painting and Printing</li>
+              </div>
+            </div>
+
+            <div class="row justify-content-center pt-3">
+                         <div class="col-8">
+                         <h4>TECHNOLOGY (DIGITAL LITERACY)</h4>
+              <li>  Animated Content Based Stories and Songs/Rhymes</li>
+              <li>  Digital Flashcards</li>
+              <li>   Digital Interactive Games</li>
+            </div>
+            </div>
+
+            <div class="row justify-content-center pt-3">
+                         <div class="col-8">
+                         <h4>ENRICHMENT PROGRAMME</h4>
+                         
+              <li>  Stories come Alive….dramatics, story yoga, puppetry, musical plays, and make-believe situations. Role Play, dramatics in Early Years is an important part of child development; it nurtures imagination, builds confidence, creativity communication, physical development and problem solving.</li>
+              <li>  Fun and Fitness - Indoor and Outdoor Fitness Programme</li>
+              <li>  Full STEAM Ahead : Little Scientists at Work</li>
+            </div>
+            </div>
+                         
+                   
+            </div>
+        </div>
+    </div>
+
+<br>
+<br>
+    
+        <div class="row mt-5 justify-content-center">
+
+          <div class="col-md-8">
+        
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+    <?php
+    include("include/footer.php");
+    ?>
+
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+  </body>
+
+</html>
+
