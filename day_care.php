95e8d1a8bcd87eb05f3b522a7025b0cb3aca5e86
Madhuri Dere <madhuridere2018@gmail.com>
2022-08-05 15:27:19 +0530

    chnages

--- day_care.php
+++ day_care.php
@@ -50,7 +50,7 @@
      ?>
 
          
-<div class="site-section-cover overlay"  style="background-image: url('images/hero_1.jpg')">
+<div class="site-section-cover overlay"  style="background-image: url('images/cute.jpg')">
         <div class="container">
           <div class="row align-items-center ">
 
