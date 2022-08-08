6a70e77536c2581d6b53c7d52222e7e3d43e5104
monikagore <monikagore6@gmail.com>
2022-08-08 18:42:57 +0530

    map width change

--- contact.php
+++ contact.php
@@ -183,7 +183,8 @@ include("config.php");
         
             <span class="text-cursive h5 text-red d-block" style="text-align:center;">School on Google map</span>
             <h2 class="text-black" style="text-align:center;">Map</h2>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d236.71594153127643!2d73.02043860886435!3d18.961713403645668!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c37460a5030b%3A0x397ba6a0f423cf78!2sKinder%20Craze%20Schools!5e1!3m2!1sen!2sin!4v1659610588867!5m2!1sen!2sin" width="1680" height="500" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d236.71594153127643!2d73.02043860886435!3d18.961713403645668!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c37460a5030b%3A0x397ba6a0f423cf78!2sKinder%20Craze%20Schools!5e1!3m2!1sen!2sin!4v1659610588867!5m2!1sen!2sin" width="1680" height="500" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style="
+    width: -webkit-fill-available;"></iframe>
             
          
         </div>
